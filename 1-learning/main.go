8d52dcd82fa794ea9f322853a7e442578542f58b
Mehmet Tarhan <hi@memtarhan.com>
2022-01-13 23:24:32 +0000

    [Learning Go] - Variables and structs

--- 1-learning/main.go
+++ 1-learning/main.go
@@ -1,37 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
 
-func main() {
-	fmt.Println("Here is Go")
+/// Scopes
+// var s string // Declare
+var s = "seven" // Declare & Initialize
 
-	var whatToSay string
-	whatToSay = "Goodbye, cruel world"
+/// Structs
 
-	var i int
-	i = 2021
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+	isYoung     bool
 
-	fmt.Println(whatToSay)
-	fmt.Println("i is set to:", i)
-
-	whatWasSaid := saySomething()
-
-	fmt.Println("The function returned:", whatWasSaid)
-
-	name, age, country := introduceYourself()
-	var ageString = fmt.Sprintf("%d", age)
-
-	var introduction = "My name is " + name + " I am " + ageString + " years old and live in " + country
-	fmt.Println(introduction)
+	/*
+		Ok, what's with the capitals?
+			In Go, if we want to have a variable available outside of the current package(Here the package is main)
+			we capitalize it. Otherwise it would be private to current package.
+			In above example:
+			Age is a public variable where isYoung is private
+	*/
 }
 
-func saySomething() string {
-	return "Something"
+var birthDate time.Time
+
+func main() {
+	// fmt.Println("Here is Go")
+
+	// var whatToSay string
+	// whatToSay = "Goodbye, cruel world"
+
+	// var i int
+	// i = 2021
+
+	// fmt.Println(whatToSay)
+	// fmt.Println("i is set to:", i)
+
+	// whatWasSaid := saySomething()
+
+	// fmt.Println("The function returned:", whatWasSaid)
+
+	// name, age, country := introduceYourself()
+	// var ageString = fmt.Sprintf("%d", age)
+
+	// var introduction = "My name is " + name + " I am " + ageString + " years old and live in " + country
+	// fmt.Println(introduction)
+
+	// var s2 = "six"
+	// s := "eight"
+
+	// log.Println("s is", s)
+	// log.Println("s2 is", s2)
+
+	// saySomething(s2)
+
+	user := User{
+		FirstName: "Mehmet",
+		LastName:  "Tarhan",
+	}
+
+	log.Println(user.BirthDate)
+
+}
+
+func saySomething(s3 string) (string, string) {
+	log.Println("s from the saySomething func is", s)
+	return s3, "Something"
 }
 
 func introduceYourself() (name string, age int, country string) {
 	var mName = "Mehmet"
-	var mAge = 2021 - 1995 
+	var mAge = 2021 - 1995
 	var mCountry = "Portugal"
 
 	return mName, mAge, mCountry
