1383d91d813ad2560c70cc7098756673aacbf54e
Mehmet Tarhan <hi@memtarhan.com>
2025-12-05 10:02:35 +0300

    <developer's guide>: custom type declaration

--- 8-developers-guide/cards/main.go
+++ 8-developers-guide/cards/main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Size of Spades")
 
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
-
-	// fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
