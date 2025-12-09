df9640b52b0affcd59c20a744c8effc366c27c17
Mehmet Tarhan <hi@memtarhan.com>
2025-12-09 10:43:25 +0300

    <developer's guide>: saving to file

--- 8-developers-guide/cards/main.go
+++ 8-developers-guide/cards/main.go
@@ -1,10 +1,16 @@
 package main
 
 func main() {
+	// cards := newDeck()
+
+	// hand, remaining := deal(cards, 5)
+
+	// hand.print()
+	// remaining.print()
 	cards := newDeck()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("cards")
 
-	hand, remaining := deal(cards, 5)
-
-	hand.print()
-	remaining.print()
+	newCards := newDeckFromFile("cards")
+	newCards.print()
 }
